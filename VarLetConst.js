eb7a576f1bdee9d0b3c77f2795f2bbb975c9ce98
Mohit kumar <124328700+Mohitkumar1322@users.noreply.github.com>
2025-11-24 14:02:13 +0530

    Add VarLetConst.js (via extension)

---
+++ VarLetConst.js
@@ -0,0 +1,10 @@
+var a=4;
+var a="mohit";
+console.log(a);//output-> mohit as changes can be happend
+
+let a = {
+  name:"mohit",
+  last_name:"kumar"
+};
+
+console.log(a.last_name+" "+typeof(a));
