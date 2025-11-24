cbba0411a8ecadad86566dd2a6d62d4642c3330d
Mohit kumar <124328700+Mohitkumar1322@users.noreply.github.com>
2025-11-24 14:04:56 +0530

    Add temporal_dead_zone.js (via extension)

---
+++ temporal_dead_zone.js
@@ -0,0 +1,14 @@
+//Temporal DEad Zone ->Jab tak let/const declare nahi hote, tab tak JS unko “death zone” me rakhta hai.
+//Access karoge → error.
+
+console.log(a);
+
+//this space is temporal dead Zone
+
+let a=10;
+
+
+//ReferenceError: Cannot access 'a' before initialization error we get 
+
+
+
